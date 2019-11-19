d9603c4d7c5cabe8fe7f208535ef58e1be5a7979
eightfeet <bajiao000000@163.com>
2019-11-20 00:30:04 +0800

    update demo

--- src/components/app.js
+++ src/components/app.js
@@ -44,9 +44,9 @@ class App extends Component {
 						<a onClick={this.handleMenu('/view')}>View</a>
 					</nav>
 				</div>
-				<div style="padding:1rem; text-align:center" onClick={this.handleCount}>handleRedux</div>
+				<div style="padding:1rem; text-align:center" onClick={this.handleCount}>handleReduxCounter</div>
 				<Router history={history} onChange={this.handleRoute}>
-					<Home path="/" redux={`this.props.redux ${this.props.count}`} />
+					<Home path="/" redux={`counter from reactRedux ${this.props.count}`} />
 					<List path="/list" />
 					<View path="/view" />
 				</Router>
